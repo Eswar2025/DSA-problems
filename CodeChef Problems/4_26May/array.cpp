6178dda825234b29972a92ad9ba885336d72033e
YDK <dineshkrishnayeturi@gmail.com>
2025-05-26 07:23:57 +0530

    Added Question images for 26may

---
+++ CodeChef Problems/4_26May/array.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+using namespace std;
+
+int main()  {
+    /* Write your code here and for furthur clarity on question please refer to explanation image from this folder */
+    // If you need the basic template (like in hackerrank) scroll down..
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    // Write your code here
+}
+
+int main() {
+    int nums1Size, nums2Size;
+    cin >> nums1Size >> nums2Size;
+
+    vector<int> nums1(nums1Size), nums2(nums2Size);
+    for (int i = 0; i < nums1Size; ++i) {
+        cin >> nums1[i];
+    }
+    for (int i = 0; i < nums2Size; ++i) {
+        cin >> nums2[i];
+    }
+
+    vector<int> result = intersect(nums1, nums2);
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+*/
